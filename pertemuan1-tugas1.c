62ed8c039412fdbc4bb30adbea7c6a293716fc82
unknown <dwywulandary10@gmail.com>
2023-10-25 22:28:06 +0700

    first commit

---
+++ pertemuan1-tugas1.c
@@ -0,0 +1,14 @@
+
+	#include <stdio.h>
+
+int main() {
+  char nim[10] = "2341071";
+  char nama[30] = "Dwy Wulandary";
+ char jurusan[30] = "Informatika";
+
+    printf("NIM: %s\n", nim);
+    printf("NAMA: %s\n", nama);
+    printf("JURUSAN: %s\n", jurusan);
+
+    return 0;
+}
